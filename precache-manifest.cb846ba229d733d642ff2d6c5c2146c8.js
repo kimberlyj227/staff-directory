0d60b95bef648e7bf7e29ae1d00f5bd8907f5afe
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-08-25 20:10:57 -0600

    Updates

---
+++ precache-manifest.cb846ba229d733d642ff2d6c5c2146c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74d042667b49c731b7e1b677e84d42c",
+    "url": "/staff-directory/index.html"
+  },
+  {
+    "revision": "6a95b399b21026a1dfc8",
+    "url": "/staff-directory/static/css/main.9a864e20.chunk.css"
+  },
+  {
+    "revision": "e72780047da70153ac4c",
+    "url": "/staff-directory/static/js/2.3010b6ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/staff-directory/static/js/2.3010b6ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a95b399b21026a1dfc8",
+    "url": "/staff-directory/static/js/main.9391984d.chunk.js"
+  },
+  {
+    "revision": "a2e333993de745f8f15c",
+    "url": "/staff-directory/static/js/runtime-main.c28052da.js"
+  }
+]);
